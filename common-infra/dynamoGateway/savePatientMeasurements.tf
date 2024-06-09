af5551026b628461ac9bb0f1bee116c7ec958903
Lucas Irineu <lhirineu@gmail.com>
2024-06-09 17:34:54 -0300

    chore: creating save measures correctly

--- common-infra/dynamoGateway/savePatientMeasurements.tf
+++ common-infra/dynamoGateway/savePatientMeasurements.tf
@@ -1,14 +1,17 @@
-data "aws_lambda_function" "existingSaveMeasurements" {
+resource "aws_lambda_alias" "savePatientInfoAlias" {
+  name = "parallax-umbra-save-measurements-alias-${var.environment_name}"
+  description = "Alias for querying patient info"
   function_name = "parallax-umbra-save-measurements-${var.environment_name}"
+  function_version = "$LATEST"
 }
 
 resource "aws_lambda_permission" "saveMeasurementsPermission" {
-  statement_id = "AllowExecutionFromAPIGatewayAuthorizer"
+  statement_id = "AllowExecutionFromAPIGateway"
   action = "lambda:InvokeFunction"
-  function_name = data.aws_lambda_function.existingSaveMeasurements.function_name
+  function_name = "${aws_lambda_alias.savePatientInfoAlias.function_name}:${aws_lambda_alias.savePatientInfoAlias.name}"
   principal = "apigateway.amazonaws.com"
   source_arn = "${aws_apigatewayv2_api.patientInfoGateway.execution_arn}/*/*/*"
-  qualifier = data.aws_lambda_function.existingSaveMeasurements.version
+  qualifier = aws_lambda_alias.savePatientInfoAlias.name
 }
 
 resource "aws_apigatewayv2_integration" "saveMeasurementsIntegration" {
@@ -17,13 +20,13 @@ resource "aws_apigatewayv2_integration" "saveMeasurementsIntegration" {
   passthrough_behavior = "WHEN_NO_MATCH"
   description = "Getting patient info"
   integration_method = "POST"
-  integration_uri = data.aws_lambda_function.existingSaveMeasurements.invoke_arn
+  integration_uri = aws_lambda_alias.savePatientInfoAlias.name
   payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_route" "saveMeasurements" {
   api_id = aws_apigatewayv2_api.patientInfoGateway.id
-  route_key = "POST /savemeasurements"
+  route_key = "GET /savemeasurements"
   target = "integrations/${aws_apigatewayv2_integration.saveMeasurementsIntegration.id}"
 
 /*   authorizer_id = aws_apigatewayv2_authorizer.patientInfoAuthorizer.id
