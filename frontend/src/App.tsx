695814417d83caaaf53b8ca6980aa513df1a7d46
Lucas Irineu <lhirineu@gmail.com>
2024-06-11 19:56:17 -0300

    chore: wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import SideDrawer from "./components/SideDrawer";
-import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
 import axios from "axios";
 
 type HeartRateResponse = {
@@ -14,16 +24,24 @@ type HeartRateResponse = {
 
 function App() {
   const userInfoEndpoint: string = import.meta.env.VITE_USER_INFO_ENDPOINT;
-  //const name = "Mirosmar";
-  const [patientInfo, setPatientInfo] = useState<HeartRateResponse>({ response: [] });
-
-    useEffect(() => {
-    axios
-      .get(`${userInfoEndpoint}/retrieveUserInfo?patientName=${name}}`)
-      .then((response) => {
-        setPatientInfo(response.data);
-      });
+  const name = "Mirosmar";
+  const [patientInfo, setPatientInfo] = useState<HeartRateResponse>({
+    response: [],
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Add async keyword here
+      await axios
+        .get(`${userInfoEndpoint}/querypatientinfo?patientName=${name}`)
+        .then((response) => {
+          setPatientInfo(response.data);
+        });
+    };
+
+    fetchData(); // Call the async function
+  }, []); // Add empty dependency array
+
   const drawerWidth = 240;
   const [isClosing, setIsClosing] = useState(false);
   const [mobileOpen, setMobileOpen] = useState(false);
@@ -48,33 +66,36 @@ function App() {
           marginTop: "64px",
           minHeight: `100vh`,
           minWidth: "100vw",
-          display: "flex"
+          display: "flex",
         }}
       >
-        <Box sx={{marginLeft: "48px", marginTop: `${48}px`}}>
+        <Box sx={{ marginLeft: "48px", marginTop: `${48}px` }}>
           <Typography variant="h3">Welcome, {userName}</Typography>
+
+          <TableContainer component={Paper} sx={{ marginTop: '32px' }}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Practitioner Name</TableCell>
+                  <TableCell>Patient Name</TableCell>
+                  <TableCell>Average Heart Rate</TableCell>
+                  <TableCell>Heart Rate</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {patientInfo.response.map((item, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{item.practitionerName}</TableCell>
+                    <TableCell>{item.patientName}</TableCell>
+                    <TableCell>{item.heartRate.reduce((a, b) => a + b) / item.heartRate.length}</TableCell>
+                    <TableCell>{item.heartRate.join(", ")}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       </Box>
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Practitioner Name</TableCell>
-            <TableCell>Patient Name</TableCell>
-            <TableCell>Heart Rate</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {patientInfo.response.map((item, index) => (
-            <TableRow key={index}>
-              <TableCell>{item.practitionerName}</TableCell>
-              <TableCell>{item.patientName}</TableCell>
-              <TableCell>{item.heartRate.join(', ')}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
     </>
   );
 }
